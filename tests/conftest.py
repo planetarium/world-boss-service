d1a8e01a4959327f863e57a48b81603ea20ab7aa
Yang Chun Ung <qooraven@gmail.com>
2022-12-29 10:35:38 +0900

    WIP

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import typing
-from decimal import Decimal
 
 import pytest
 import sqlalchemy as sa
@@ -9,7 +8,7 @@ from pytest_postgresql.janitor import DatabaseJanitor
 from pytest_redis import factories  # type: ignore
 
 from world_boss.app.config import config
-from world_boss.app.models import Transaction, WorldBossReward, WorldBossRewardAmount
+from world_boss.app.models import Transaction
 from world_boss.app.stubs import RewardDictionary
 from world_boss.wsgi import create_app
 
@@ -61,36 +60,6 @@ def fx_session(fx_app):
 def fx_test_client(fx_app: Flask) -> FlaskClient:
     fx_app.testing = True
     return fx_app.test_client()
-
-
-@pytest.fixture()
-def fx_world_boss_reward_amounts(fx_session) -> typing.List[WorldBossRewardAmount]:
-    reward = WorldBossReward()
-    reward.avatar_address = "avatar_address"
-    reward.agent_address = "agent_address"
-    reward.raid_id = 1
-    reward.ranking = 1
-    result = []
-    fx_session.add(reward)
-    i = 1
-    for ticker, decimal_places in [("CRYSTAL", 18), ("RUNE_FENRIR1", 0)]:
-        reward_amount = WorldBossRewardAmount()
-        reward_amount.amount = Decimal("10")
-        reward_amount.ticker = ticker
-        reward_amount.decimal_places = decimal_places
-        reward_amount.reward = reward
-        tx_id = str(i)
-        reward_amount.tx_id = tx_id
-        transaction = Transaction()
-        transaction.tx_id = tx_id
-        transaction.signer = "signer"
-        transaction.payload = f"10 {ticker}"
-        transaction.nonce = i
-        fx_session.add(transaction)
-        result.append(reward_amount)
-        i += 1
-    fx_session.commit()
-    return result
 
 
 @pytest.fixture(scope="session")
